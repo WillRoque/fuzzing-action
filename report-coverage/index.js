8d9d39bd25a78cf2fa019895009ac4fa984dd463
Willian Roque <willianfernandoroque@gmail.com>
2020-08-13 11:44:54 +0200

    add report-coverage action

---
+++ report-coverage/index.js
@@ -0,0 +1,29 @@
+const core = require("@actions/core");
+const { execSync } = require("child_process");
+const { GitHub, context } = require("@actions/github");
+
+const main = async () => {
+    const githubToken = core.getInput("github-token");
+    const githubClient = new GitHub(githubToken);
+
+    if (!context.payload.pull_request) {
+        core.info("Not creating a comment with test coverage because this is not a pull request.")
+        return;
+    }
+
+    const commentBody = `<p>Total Coverage: <code>85%</code></p>
+<details><summary>Coverage report</summary>
+<p>
+<pre>test</pre>
+</p>
+</details>`;
+
+    await githubClient.issues.createComment({
+        repo: context.repo.repo,
+        owner: context.repo.owner,
+        issue_number: context.payload.pull_request.number,
+        body: commentBody,
+    });
+};
+
+main().catch(err => core.setFailed(err.message));

57807d609e6bd8b729c809729fd26e35c9bb868e
Willian Roque <willianfernandoroque@gmail.com>
2020-08-13 14:41:47 +0200

    change imports

--- report-coverage/index.js
+++ report-coverage/index.js
@@ -1,28 +1,30 @@
-import core from "@actions/core"
-import { GitHub, context } from "@actions/github"
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 async function main() {
-    const githubToken = core.getInput("github-token");
-    const githubClient = new GitHub(githubToken);
+  const githubToken = core.getInput("github-token");
+  const githubClient = new github.getOctokit(githubToken);
 
-    if (!context.payload.pull_request) {
-        core.info("Not creating a comment with test coverage because this is not a pull request.")
-        return;
-    }
+  if (!github.context.payload.pull_request) {
+    core.info(
+      "Not creating a comment with test coverage because this is not a pull request."
+    );
+    return;
+  }
 
-    const commentBody = `<p>Total Coverage: <code>85%</code></p>
+  const commentBody = `<p>Total Coverage: <code>85%</code></p>
 <details><summary>Coverage report</summary>
 <p>
 <pre>test</pre>
 </p>
 </details>`;
 
-    await githubClient.issues.createComment({
-        repo: context.repo.repo,
-        owner: context.repo.owner,
-        issue_number: context.payload.pull_request.number,
-        body: commentBody,
-    });
+  await githubClient.issues.createComment({
+    repo: github.context.repo.repo,
+    owner: github.context.repo.owner,
+    issue_number: github.context.payload.pull_request.number,
+    body: commentBody,
+  });
 }
 
-main().catch(err => core.setFailed(err.message));
+main().catch((err) => core.setFailed(err.message));
